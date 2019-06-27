fc62ee2f183811c2d67990567e23a8789e8d62c5
NevGibson <nev@indivision.ca>
2019-06-27 12:01:01 -0700

    add ais and powercycle functionality to photobot commandline

---
+++ photobot_src/powercycle.py
@@ -0,0 +1,27 @@
+"""
+ptz Camera version of photobot
+
+TODO/Sort out
+- python 2.7 only
+- we need the wsdl dir
+- we need the network location for the ptz, we might
+  need to scan for that??
+"""
+
+
+import subprocess
+from datetime import datetime
+import time as timer
+import os
+import sys
+import logging
+from lorex import LorexCam
+import argparse
+from configparser import ConfigParser
+
+from photobot_helpers import *
+
+################################################################################
+# beginning of main execution
+if __name__=="__main__":
+    power_cycle(5)
