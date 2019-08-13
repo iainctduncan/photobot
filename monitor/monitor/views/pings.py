4960eca0cbfc29ba4ffd580502013cd78886d8f7
NevGibson <nev@indivision.ca>
2019-08-13 11:34:31 -0700

    update to show more pings

--- monitor/monitor/views/pings.py
+++ monitor/monitor/views/pings.py
@@ -14,7 +14,7 @@ def pings_by_install(request):
     install_id = request.matchdict.get('install')
     log.info(install_id)
     # the installations shows the latest ping from all active installations
-    pings = request.dbsession.query(Ping).filter(Ping.installation_uid==install_id).order_by(Ping.datetime.desc()).limit(50).all()
+    pings = request.dbsession.query(Ping).filter(Ping.installation_uid==install_id).order_by(Ping.datetime.desc()).limit(150).all()
 
     return dict(pings=pings)
 
@@ -24,7 +24,7 @@ def pings_by_subsystem(request):
     subsystem = request.matchdict.get('subsystem')
     log.info(install_id)
     # the installations shows the latest ping from all active installations
-    pings = request.dbsession.query(Ping).filter(Ping.installation_uid==install_id).filter(Ping.subsystem==subsystem).order_by(Ping.datetime.desc()).limit(50).all()
+    pings = request.dbsession.query(Ping).filter(Ping.installation_uid==install_id).filter(Ping.subsystem==subsystem).order_by(Ping.datetime.desc()).limit(150).all()
 
     return dict(pings=pings)
 
@@ -33,6 +33,6 @@ def pings_by_subsystem(request):
 def pings(request):
 
     # the installations shows the latest ping from all active installations
-    pings = request.dbsession.query(Ping).order_by(Ping.datetime.desc()).limit(50).all()
+    pings = request.dbsession.query(Ping).order_by(Ping.datetime.desc()).limit(150).all()
 
     return dict(pings=pings)
