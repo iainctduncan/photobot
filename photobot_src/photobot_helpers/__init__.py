080d52808577f79226465151f93454ab601a81b8
NevGibson <nev@indivision.ca>
2019-08-12 19:21:50 -0700

    add rotation to thermal images convert to string

--- photobot_src/photobot_helpers/__init__.py
+++ photobot_src/photobot_helpers/__init__.py
@@ -314,7 +314,7 @@ def capture_thermal_image():
         latest_image_path = os.path.abspath(os.readlink(target + "/latest.png"))
         final_path = target + "/" + get_photo_filename(settings['installation_id'],'thermal','png')
         os.rename(latest_image_path,final_path)
-        rotation_degrees = settings.get('thermal_rotation_degrees',180)
+        rotation_degrees = str(settings.get('thermal_rotation_degrees',180))
         if rotation_degrees:
             os.system("mogrify -rotate" + rotation_degrees + " " + final_path)
         log_latest_photo_path(final_path,"thermal")
